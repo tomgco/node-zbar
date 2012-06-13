48ec3853aa54ed792d8e06a23837711db888ea43
Tom Gallacher <tomgallacher23@gmail.com>
2012-06-13 17:40:34 +0100

    Listen to error events in example

--- test/example.js
+++ test/example.js
@@ -2,6 +2,10 @@ var Zbar = require('../zbar')
   , zbar = new Zbar('/dev/video0')
   ;
 
-zbar.on('data', function(data) {
-  console.log(data);
+zbar.on('data', function(buf) {
+  console.log(buf.toString());
+});
+
+zbar.on('err', function(buf) {
+  console.log(buf.toString());
 });
